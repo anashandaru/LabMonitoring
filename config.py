304991a627b7a9cf996bb618e39eca137d8825d5
anashandaru <anashandaru256@gmail.com>
2021-09-20 15:28:47 +0700

    add configfile

---
+++ config.py
@@ -0,0 +1,8 @@
+labName = 'Lab Batuan'
+
+firebaseConfig ={
+    "apiKey":"AIzaSyCsSxN_pAQbl_ePNSyRY8oJhx_mtvTAA3o",
+    "authDomain": "labmon-5dd47.firebaseapp.com",
+    "databaseURL":"https://labmon-5dd47-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    "storageBucket": "labmon-5dd47.appspot.com"
+}
